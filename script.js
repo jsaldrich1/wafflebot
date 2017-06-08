6706f43ce132c6efb8f91917ac914dc5889484a9
jsaldrich1 <jsaldrich1@gmail.com>
2017-06-07 21:28:30 -0400

    just loads of css nonsense, still WIP

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function serveMeWaffles(){
   function getNumber( min, max ) {
     return Math.floor( Math.random() * ( max - min + 1 )) + min;
   };
-  document.getElementById("random-waffles").src="pics/" + waffleMessage + ".png";
+  document.getElementById("randomWaffles").src="pics/" + waffleMessage + ".png";
 };
 
 $(document).ready(function() {
   serveMeWaffles();
 
-  $("#random-waffles").click(function() {
+  $("#randomWaffles").click(function() {
     serveMeWaffles();
   });
 });
